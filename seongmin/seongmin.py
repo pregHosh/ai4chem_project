45a91f1e3946b57020143caff80e9970f3203f2b
Seongmin-Jeon <seongmin.jeon.@epfl.ch>
2024-05-01 01:48:05 +0200

    molecule validity test

--- seongmin/seongmin.py
+++ seongmin/seongmin.py
@@ -1,6 +1,23 @@
 pip install rdkit
 from rdkit import Chem
 import pandas as pd
+from rdkit.Chem import PandasTools
 
 df = pd.read_csv("../data/iflp_smiles.csv")
+
+PandasTools.AddMoleculeColumnToFrame(df, smilesCol='smiles', molCol='Molecule')
+#df.head()
+
+#Check if mol are valid
+def mol_valid(molecule):
+    val = True
+    flag = Chem.SanitizeMol(molecule)
+    if flag == Chem.SanitizeFlags.SANITIZE_NONE:
+        val = True
+    else:
+        val = False
+    return val
+
+df['sanitize_flag']=df['mol'].apply(mol_valid)
+
 df.head()
