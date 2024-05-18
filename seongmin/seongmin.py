5169c4b03b3447d55852740b0d8960c727fde61a
Seongmin-Jeon <seongmin.jeon.@epfl.ch>
2024-05-18 12:08:36 +0200

    FEPA FEHA plot

--- seongmin/seongmin.py
+++ seongmin/seongmin.py
@@ -1,10 +1,13 @@
 from rdkit import Chem
-
+#import io
 import pandas as pd
-from rdkit.Chem import PandasTools
+from rdkit import Chem
+from rdkit.Chem import Draw
+from utils import plot_3d, plot_2d
+
 
 df = pd.read_csv('../data/iflp_dataset_ed_idxs.csv')
-df.head()
+#df.head()
 
 #PandasTools.AddMoleculeColumnToFrame(df, smilesCol='smiles', molCol='Molecule')
 #df.head()
@@ -19,6 +22,7 @@ def mol_valid(molecule):
         val = False
     return val
 
-#df['sanitize_flag']=df['mol'].apply(mol_valid)
+df['sanitize_flag']=df['mol'].apply(mol_valid)
 
-#df.head()
+
+# Plot the FEHA and FEPA
